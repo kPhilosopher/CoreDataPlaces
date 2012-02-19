a8536f7338e0fbc103cfdc1c7ce897d990757094
Jinwoo <agreee45@hotmail.com>
2012-02-18 22:45:00 -0800

    iPad compatible. Also, in the progress of adding notification for alerts

---
+++ CoreDataPlaces/CPNotificationManager.h
@@ -0,0 +1,13 @@
+//
+//  CPNotificationManager.h
+//  CoreDataPlaces
+//
+//  Created by Jinwoo Baek on 2/18/12.
+//  Copyright (c) 2012 Rose-Hulman Institute of Technology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CPNotificationManager : NSObject
+
+@end
---
+++ CoreDataPlaces/CPNotificationManager.m
@@ -0,0 +1,13 @@
+//
+//  CPNotificationManager.m
+//  CoreDataPlaces
+//
+//  Created by Jinwoo Baek on 2/18/12.
+//  Copyright (c) 2012 Rose-Hulman Institute of Technology. All rights reserved.
+//
+
+#import "CPNotificationManager.h"
+
+@implementation CPNotificationManager
+
+@end
