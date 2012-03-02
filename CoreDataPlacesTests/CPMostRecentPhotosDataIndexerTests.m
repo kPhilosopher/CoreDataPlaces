05fe882452a7d744b31975bbe3927d94dd8a7ad9
Jinwoo <agreee45@hotmail.com>
2012-03-01 23:31:54 -0800

    Refactored.

--- CoreDataPlacesTests/CPMostRecentPhotosDataIndexerTests.m
+++ CoreDataPlacesTests/CPMostRecentPhotosDataIndexerTests.m
@@ -62,11 +62,18 @@
 
 - (void)testMethod_refineTheRawElementThenAddToTemporaryMutableArray_01;
 {
+	//setup
 	NSMutableArray *testOutcome = [NSMutableArray array];
 	CPMostRecentPhotosRefinedElement *refinedElementForDataIndexer = [[CPMostRecentPhotosRefinedElement alloc] init];
 	CPMostRecentPhotosDataIndexer *dataIndexer = [[CPMostRecentPhotosDataIndexer alloc] initWithRefinedElement:refinedElementForDataIndexer];
+	
+	//method under test.
 	[dataIndexer refineTheRawElement:nil thenAddToTemporaryMutableArray:testOutcome];
+	
+	//evaluate the outcome.
 	STAssertTrue(([[testOutcome lastObject] isKindOfClass:[refinedElementForDataIndexer class]]),@"The refined element should be of the correct class");
+	
+	//clean up
 	[refinedElementForDataIndexer release]; refinedElementForDataIndexer = nil;
 }
 
