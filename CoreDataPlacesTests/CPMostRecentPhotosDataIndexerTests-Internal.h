0778cc1464b5aecd52a81f625ba41069ea418d00
Jinwoo <agreee45@hotmail.com>
2012-03-03 17:28:19 -0800

    Corrected the company name.

--- CoreDataPlacesTests/CPMostRecentPhotosDataIndexerTests-Internal.h
+++ CoreDataPlacesTests/CPMostRecentPhotosDataIndexerTests-Internal.h
@@ -3,7 +3,7 @@
 //  CoreDataPlaces
 //
 //  Created by Jinwoo Baek on 3/1/12.
-//  Copyright (c) 2012 Rose-Hulman Institute of Technology. All rights reserved.
+//  Copyright (c) 2012 Jinwoo Baek. All rights reserved.
 //
 
 // !!!: Testing using CPMostRecentPhotosRefinedElement
