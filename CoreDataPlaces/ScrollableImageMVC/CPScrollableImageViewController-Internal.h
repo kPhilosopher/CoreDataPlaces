a28f450ad57dc937442c1713846e9423b8125371
Jinwoo <agreee45@hotmail.com>
2012-03-06 00:31:53 -0800

    zooming fix.

---
+++ CoreDataPlaces/ScrollableImageMVC/CPScrollableImageViewController-Internal.h
@@ -0,0 +1,18 @@
+//
+//  CPScrollableImageViewController-Internal.h
+//  CoreDataPlaces
+//
+//  Created by Jinwoo Baek on 3/5/12.
+//  Copyright (c) 2012 Rose-Hulman Institute of Technology. All rights reserved.
+//
+
+#import "CPScrollableImageViewController.h"
+
+@interface CPScrollableImageViewController()
+
+#pragma mark - Internal method
+
+- (CGRect)CP_getTheRectSizeThatWillUtilizeTheScreenSpace;
+- (void)CP_setTheZoomScales;
+
+@end
