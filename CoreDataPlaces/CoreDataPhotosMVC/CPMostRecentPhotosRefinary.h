b5657613d6b4b4b31fc38b17d69d9ca87f459645
Jinwoo <agreee45@hotmail.com>
2012-03-19 04:58:56 -0700

    Changed the directory.

---
+++ CoreDataPlaces/CoreDataPhotosMVC/CPMostRecentPhotosRefinary.h
@@ -0,0 +1,14 @@
+//
+//  CPMostRecentPhotosRefinary.h
+//  CoreDataPlaces
+//
+//  Created by Jinwoo Baek on 3/2/12.
+//  Copyright (c) 2012 Jinwoo Baek. All rights reserved.
+//
+
+#import "CPCoreDataPhotosRefinary.h"
+
+
+@interface CPMostRecentPhotosRefinary : CPCoreDataPhotosRefinary
+
+@end
