e40e09d88fcbeb9dd819724ebf632104c5fe68bb
Jinwoo <agreee45@hotmail.com>
2012-03-20 14:16:00 -0700

    Refactoring.

--- CoreDataPlaces/TopPlacesTableMVC/CPTopPlacesTableViewController.h
+++ CoreDataPlaces/TopPlacesTableMVC/CPTopPlacesTableViewController.h
@@ -9,24 +9,9 @@
 #import "CPIndexedTableViewController.h"
 
 
-@class CPFlickrDataSource;
-
 @interface CPTopPlacesTableViewController : CPIndexedTableViewController
 
 extern NSString *CPTopPlacesTableViewAccessibilityLabel;
-
-#pragma mark - Property
-
-//@property (retain) NSArray *listOfPlaces;
-//@property (retain) NSMutableArray *indexedListOfPlaces;
-//@property (retain) id<CPTableViewControllerDataReloading> delegateToUpdateMostRecentPlaces;
-//@property (retain) CPFlickrDataSource *flickrDataSource;
-//@property (assign) id<PictureListTableViewControllerDelegate> delegateToTransfer;
-
-#pragma mark - Initialization
-
-//- (id)initWithStyle:(UITableViewStyle)style dataIndexHandler:(id<CPDataIndexHandling>)dataIndexHandler tableViewHandler:(id<CPTableViewHandling>)tableViewHandler theFlickrDataSource:(CPFlickrDataSource *)theFlickrDataSource;
-//- (id)initWithStyle:(UITableViewStyle)style tableViewHandler:(CPTableViewHandler *)tableViewHandler dataHandler:(CPDataHandler *)dataHandler managedObjectContext:(NSManagedObjectContext *)managedObjectContext;
 
 #pragma mark - Factory method
 
