9cd3366b752b811d1530a3fea5f0db0ef3855435
Jinwoo <agreee45@hotmail.com>
2012-03-20 14:26:10 -0700

    Added condition to reduce uneccesary step.

--- CoreDataPlaces/IndexedTableMVC/CPDataIndexer.m
+++ CoreDataPlaces/IndexedTableMVC/CPDataIndexer.m
@@ -16,28 +16,30 @@
 
 - (NSMutableArray *)indexedSectionsOfRefinedElements:(NSArray *)refinedElements;
 {
-	NSMutableArray *theElementSections = [[[NSMutableArray alloc] init] autorelease];
-	NSMutableArray *temporaryDataElements = [NSMutableArray arrayWithArray:refinedElements];
-	
-	//TODO: in the controller that calls the refinary, the raw elements should be checked to not be nil and have at least one element in it.
-	//1. set the sections number for each element
-	NSInteger highSection = [self sectionsCountAndSetSectionNumberForElementsInArray:temporaryDataElements];
-	
-	//2. create the sectionsArray
-	NSMutableArray *indexedSections = [NSMutableArray arrayWithCapacity:highSection];
-	
-	//3. make the empty arrays for each sections.
-	for (int i = 0 ; i < highSection; i++)
-		[indexedSections addObject:[[[NSMutableArray alloc] initWithCapacity:0] autorelease]];
-	
-	//4. put elements into its section
-	for (CPRefinedElement *element in temporaryDataElements) 
-		[(NSMutableArray *)[indexedSections objectAtIndex:element.sectionNumber] addObject:element];
-	
-	//5. sort the elements within each sections
-	for (NSMutableArray *unsortedSection in indexedSections) 
-		[self sortTheElementsInSectionArray:unsortedSection andAddToArrayOfSections:theElementSections];
-	
+	NSMutableArray *theElementSections = nil;
+	if (refinedElements && refinedElements.count > 0)
+	{
+		theElementSections = [[[NSMutableArray alloc] init] autorelease];
+		NSMutableArray *temporaryDataElements = [NSMutableArray arrayWithArray:refinedElements];
+		
+		//1. set the sections number for each element
+		NSInteger highSection = [self sectionsCountAndSetSectionNumberForElementsInArray:temporaryDataElements];
+		
+		//2. create the sectionsArray
+		NSMutableArray *indexedSections = [NSMutableArray arrayWithCapacity:highSection];
+		
+		//3. make the empty arrays for each sections.
+		for (int i = 0 ; i < highSection; i++)
+			[indexedSections addObject:[[[NSMutableArray alloc] initWithCapacity:0] autorelease]];
+		
+		//4. put elements into its section
+		for (CPRefinedElement *element in temporaryDataElements) 
+			[(NSMutableArray *)[indexedSections objectAtIndex:element.sectionNumber] addObject:element];
+		
+		//5. sort the elements within each sections
+		for (NSMutableArray *unsortedSection in indexedSections) 
+			[self sortTheElementsInSectionArray:unsortedSection andAddToArrayOfSections:theElementSections];
+	}
 	return theElementSections;
 }
 
